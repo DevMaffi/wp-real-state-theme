fa44f3e9a980c9be485fa69838ef12d780789c31
devmaffi <devmaffi550@gmail.com>
2022-05-20 11:58:48 +0300

    Break index.php layout into sections

--- footer.php
+++ footer.php
@@ -7,3 +7,122 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
+
+?>
+
+<!-- FOOTER -->
+<footer class="footer section">
+
+  <!-- FOOTER CONTAINER -->
+  <div class="footer__container container grid">
+    <div>
+      <a class="footer__logo" href="#home">
+        Holux
+        <i class="bx bxs-home-alt-2"></i>
+      </a>
+      <p class="footer__description">
+        Our vision is to make all people
+        <br />
+        the best place to live for them.
+      </p>
+    </div>
+
+    <!-- FOOTER CONTENT -->
+    <div class="footer__content">
+      <div>
+        <h3 class="footer__title">About</h3>
+
+        <!-- FOOTER LINKS -->
+        <ul class="footer__links">
+          <li>
+            <a class="footer__link" href="#">About Us</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">Features</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">News &amp; Blog</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="footer__title">Company</h3>
+
+        <!-- FOOTER LINKS -->
+        <ul class="footer__links">
+          <li>
+            <a class="footer__link" href="#">How We Work?</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">Capital</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">Security</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="footer__title">Support</h3>
+
+        <!-- FOOTER LINKS -->
+        <ul class="footer__links">
+          <li>
+            <a class="footer__link" href="#">FAQs</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">Support center</a>
+          </li>
+          <li>
+            <a class="footer__link" href="#">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="footer__title">Follow us</h3>
+
+        <!-- FOOTER SOCIAL -->
+        <ul class="footer__social">
+          <li>
+            <a class="footer__social-link" href="https://www.facebook/" target="_blank">
+              <i class="bx bxl-facebook-circle"></i>
+            </a>
+          </li>
+          <li>
+            <a class="footer__social-link" href="https://www.instagram.com/" target="_blank">
+              <i class="bx bxl-instagram-alt"></i>
+            </a>
+          </li>
+          <li>
+            <a class="footer__social-link" href="https://www.pinterest.com/" target="_blank">
+              <i class="bx bxl-pinterest"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <!-- FOOTER INFO -->
+  <div class="footer__info container">
+    <span class="footer__copy">
+      &#169; 2022 Holux. All rights reserved
+    </span>
+    <div class="footer__privacy">
+      <a href="#">Terms &amp; Agreements</a>
+      <a href="#">Privacy Policy</a>
+    </div>
+  </div>
+</footer>
+</main>
+
+<!-- SCROLL UP -->
+<a class="scroll-up" id="scroll-up" href="#home">
+  <i class="bx bx-chevrons-up"></i>
+</a>
+
+<?php
+wp_footer();
+?>
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -7,3 +7,80 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- HEAD -->
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="shortcut icon" href="<?= bloginfo('template_url'); ?>/assets/images/favicon.png" type="image/x-icon" />
+  <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+  <title>
+    <?php
+    bloginfo('name');
+    echo ' | ';
+    bloginfo('description');
+    ?>
+  </title>
+
+  <?php
+  wp_head();
+  ?>
+</head>
+
+<!-- BODY -->
+
+<body>
+
+  <!-- MAIN -->
+  <main class="main">
+
+    <!-- HEADER -->
+    <header class="header" id="header">
+
+      <!-- NAV CONTAINER -->
+      <nav class="nav container">
+        <a class="nav__logo" href="#home">
+          Holux
+          <i class="bx bxs-home-alt-2"></i>
+        </a>
+
+        <!-- NAV MENU -->
+        <div class="nav__menu">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a class="nav__link active-link" href="#home">
+                <i class="bx bx-home-alt-2"></i>
+                <span>Home</span>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link null" href="#popular">
+                <i class="bx bx-building-house"></i>
+                <span>Residences</span>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link null" href="#value">
+                <i class="bx bx-award"></i>
+                <span>Value</span>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link null" href="#contact">
+                <i class="bx bx-phone"></i>
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <i class="change__theme bx bx-moon" id="theme-button"></i>
+        <a class="button nav__button" href="#">Subscribe</a>
+      </nav>
+    </header>
